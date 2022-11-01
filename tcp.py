aefb7d2f974f1ebae02ca40b35694338af818638
MunQ-Lee <dse235@gmail.com>
2022-11-01 06:25:20 +0000

    init commit

---
+++ tcp.py
@@ -0,0 +1,50 @@
+from socket import *
+import os
+import struct
+
+class communicator():
+    class SocketInfo():
+        HOST=SERVER_NAME
+        PORT=SERVER_PORT
+        BUFSIZE=100
+        ADDR=(HOST, PORT)
+    
+    def __init__(self):
+        self.csock = socket(AF_INET, SOCK_STREAM)
+        self.csock.connect(self.SocketInfo.ADDR)
+        print("conenct is success")
+
+    def __del__(self):
+        self.csock.close()   
+
+    def send_int(self, input: int):
+        input_byte = struct.pack('<i', input)
+        self.csock.send(input_byte)
+
+    def recv_int(self):
+        recv_bytes = self.csock.recv(4)
+        recv_int = struct.unpack('<i', recv_bytes)
+        return recv_int[0]
+
+    def send_float(self, input):
+        input_byte = struct.pack('<f', input)
+        self.csock.send(input_byte)
+    
+    def recv_float(self):
+        recv_bytes = self.csock.recv(4)
+        recv_float = struct.unpack('<f', recv_bytes)
+        return recv_float[0]
+
+if __name__ == '__main__':
+    connect = communicator()
+
+    pid = os.getpid()
+    connect.send_int(pid)
+    print(f"pid is {pid}")
+
+    ret = connect.recv_int()
+    print(ret)
+
+    connect.send_float(3.14159265358979323846)
+
+    
